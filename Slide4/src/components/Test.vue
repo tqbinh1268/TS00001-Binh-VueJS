3ba895484888710e68fabf00d4cfb407f5907fa3
tqbinh1268 <tqbinh@gmail.com>
2026-01-15 09:52:17 +0700

    slide 4 ap hap

--- Slide4/src/components/Test.vue
+++ Slide4/src/components/Test.vue
@@ -1,13 +1,32 @@
 <template lang="">
     <div>
-        
+        Number: {{ count}}
+        <button @click="increment">Add</button>
+        <button @click="decrement">Sub</button>
+        <button @click="reset">Reset</button>
     </div>
 </template>
 <script>
 export default {
-    
-}
+
+    data() {
+        return {
+            count: 0
+        };
+    },
+    methods: {
+        increment() {
+            this.count++;// Dùng this để truy cập biến trong data
+        },
+        decrement() {
+            this.count--;// Dùng this để truy cập biến trong data
+        },
+        reset() {
+            this.count = 0; //đặt lại count về 0
+        }
+    }
+};
 </script>
 <style lang="">
-    
+
 </style>
