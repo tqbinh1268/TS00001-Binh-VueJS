b13725b5504effbbcc6d46a9da6fedcba80748f9
tqbinh1268 <tqbinh@gmail.com>
2026-01-29 09:23:22 +0700

    Slide 7

---
+++ Slide7/src/components/Slide7.vue
@@ -0,0 +1,16 @@
+<template lang="">
+    <div class="container">
+        <h1 @click.right="handleClick">Slide 7</h1>
+        <input type="" name="" v-model="myName" placeholder="Nhập tên" v-on:keyup.enter.shift="handleClick">
+    </div>
+</template>
+<script setup>
+import { ref } from 'vue'
+    const myName = ref('')
+    const handleClick = () => {
+alert(myName.value);
+};
+</script>
+<style lang="">
+    
+</style>
