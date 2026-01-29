49f4fd84d2ca9b1e5def45c2359fad81c0fd7d82
tqbinh1268 <tqbinh@gmail.com>
2026-01-29 12:04:13 +0700

    slide 7

--- Slide7/src/components/Slide7.vue
+++ Slide7/src/components/Slide7.vue
@@ -1,16 +1,17 @@
 <template lang="">
-    <div class="container">
-        <h1 @click.right="handleClick">Slide 7</h1>
-        <input type="" name="" v-model="myName" placeholder="Nhập tên" v-on:keyup.enter.shift="handleClick">
+    <div>
+        <h1 v-on:click.right="handleClick('Hello Vue!')">Slide 7</h1>
+        <input type="" name="" v-model.lazy.trim="myName"> <br>
+        <p>{{ myName }}</p>
     </div>
 </template>
 <script setup>
-import { ref } from 'vue'
-    const myName = ref('')
-    const handleClick = () => {
-alert(myName.value);
+import { ref } from 'vue';
+const myName = ref('');
+const handleClick = (message) => {
+    alert(myName.value);
 };
 </script>
 <style lang="">
-    
+
 </style>
